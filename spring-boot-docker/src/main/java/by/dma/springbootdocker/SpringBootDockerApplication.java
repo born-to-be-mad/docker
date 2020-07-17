876e708c3ed9f8059cfe8c166d39695335a539c9
Dzmitry Marudau <vinmaster@tut.by>
2020-07-17 16:32:05 +0300

    Set up a Spring Boot app for contarization

---
+++ spring-boot-docker/src/main/java/by/dma/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package by.dma.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello from Docker World";
+    }
+
+}
